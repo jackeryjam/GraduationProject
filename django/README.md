d9c9bc265e3ae3715192b83d541b446350ff7157
jackeryjam <jackeryjam@gmail.com>
2019-03-25 21:21:36 +0800

    segment

--- django/README.md
+++ django/README.md
@@ -1,7 +1,7 @@
 # 服务端
 
 ### django启动方式  
-python manage.py runserver --host 0.0.0.0 --port 9008  
+python manage.py runserver 0.0.0.0:8000  
 
 ### 安装
 pip install django
--- django/Server/API/segment.py
+++ django/Server/API/segment.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import json
+from FCN.infer import segment
 
 def segment_POST(request):
+    resp = {'code': 200, 'data': 'Get success'}
+    return HttpResponse(json.dumps(resp), content_type="application/json")
+
+def segment_GET(request):
+    image = request.GET.get('image')
     resp = {'code': 200, 'data': 'Get success'}
     return HttpResponse(json.dumps(resp), content_type="application/json")
 
@@ -11,4 +17,4 @@ def segment(request):
     if request.POST:
         return segment_POST(request)
     else:
-        return HttpResponse("Hello world ! ")
+        return segment_GET(request)
